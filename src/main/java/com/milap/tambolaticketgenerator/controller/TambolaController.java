57864aae26c889facb7e9a40bb30664f5ce00ee7
milap-doshi <milap.doshi@outlook.com>
2020-07-18 15:38:10 +0530

    Update TambolaController.java
    Added support for generating multiple tickets

--- src/main/java/com/milap/tambolaticketgenerator/controller/TambolaController.java
+++ src/main/java/com/milap/tambolaticketgenerator/controller/TambolaController.java
@@ -1,8 +1,8 @@
 package com.milap.tambolaticketgenerator.controller;
 
-import com.milap.tambolaticketgenerator.model.Ticket;
-import com.milap.tambolaticketgenerator.service.TicketGenerator;
-import com.milap.tambolaticketgenerator.service.ValidationService;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,26 +10,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.milap.tambolaticketgenerator.model.Ticket;
+import com.milap.tambolaticketgenerator.service.TicketGenerator;
+import com.milap.tambolaticketgenerator.service.ValidationService;
+
 @Controller
 public class TambolaController {
 
-    private ValidationService validationService;
-    private TicketGenerator ticketGenerator;
+	private ValidationService validationService;
+	private TicketGenerator ticketGenerator;
+	int Number = 1;
 
-    @Autowired
-    public TambolaController(ValidationService validationService, TicketGenerator ticketGenerator) {
-        this.validationService = validationService;
-        this.ticketGenerator = ticketGenerator;
-    }
+	@Autowired
+	public TambolaController(ValidationService validationService, TicketGenerator ticketGenerator) {
+		this.validationService = validationService;
+		this.ticketGenerator = ticketGenerator;
+	}
 
-    @PostMapping(path = "/new")
-    public ResponseEntity<Ticket> generateTicket(@RequestParam(name = "session") String session) {
-        Ticket ticket = ticketGenerator.generateNewTicket(session);
-        return new ResponseEntity<Ticket>(ticket, HttpStatus.OK);
-    }
+	@PostMapping(path = "/new")
+	public ResponseEntity<List<Ticket>> generateTicket(@RequestParam(name = "session") String session,
+			@RequestParam(value = "tickets", required = false, defaultValue = "0") int tickets) {
 
-    @PostMapping(value = "/validate/row")
-    private boolean validateRows(int rowNum, Ticket ticket) {
-        return validationService.validateRow(rowNum, ticket);
-    }
+		List<Ticket> returnList = new ArrayList<>();
+		for (int i = 0; i < tickets; i++) {
+			Ticket ticket = ticketGenerator.generateNewTicket(session);
+			returnList.add(ticket);
+		}
+		return new ResponseEntity<List<Ticket>>(returnList, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/validate/row")
+	private boolean validateRows(int rowNum, Ticket ticket) {
+		return validationService.validateRow(rowNum, ticket);
+	}
 }
