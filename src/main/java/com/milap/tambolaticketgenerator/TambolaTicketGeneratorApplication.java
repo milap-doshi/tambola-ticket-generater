8975c5d3552fed5f23cc448fb45b08ec04eb297b
milap-doshi <milap.doshi@outlook.com>
2024-01-01 14:11:53 +0530

    Converted project to gradle war

--- src/main/java/com/milap/tambolaticketgenerator/TambolaTicketGeneratorApplication.java
+++ src/main/java/com/milap/tambolaticketgenerator/TambolaTicketGeneratorApplication.java
@@ -2,9 +2,6 @@ package com.milap.tambolaticketgenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TambolaTicketGeneratorApplication {
@@ -13,13 +10,4 @@ public class TambolaTicketGeneratorApplication {
 		SpringApplication.run(TambolaTicketGeneratorApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/new").allowedOrigins("http://192.168.1.7:3000", "http://localhost:3000");
-			}
-		};
-	}
 }
